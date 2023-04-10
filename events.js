58fc5d92c0f65216c1a426cf870335419dddb30e
arnold <arnolddenzil@gmail.com>
2023-04-10 21:36:22 +0530

    added events.js - list of all events of all the depts

---
+++ events.js
@@ -0,0 +1,236 @@
+const events = {
+    cse: [
+        {
+            id: 1,
+            title: "Velocity - Type Racer",
+        },
+        {
+            id: 2,
+            title: "SPEED FREAK - NFS GAMING",
+        },
+        {
+            id: 3,
+            title: "CYBERVERSE – VR GAMING",
+        },
+        {
+            id: 4,
+            title: "FILM TRIVIA – FILM QUIZ",
+        },
+        {
+            id: 5,
+            title: "HACKATHON",
+        },
+        {
+            id: 6,
+            title: "SPIDERZ-WEB DEVELOPER COMPETITION",
+            
+        },
+        {
+            id: 7,
+            title: "TECH WIZARD",
+            
+        },
+        {
+            id: 8,
+            title: "WEBZEIGN-UI DESIGNING",
+            
+        },
+        {
+            id: 9,
+            title: "CODE MAFIA – RELAY CODING",
+            
+        },
+        {
+            id: 10,
+            title: "PATTERN PRINTING",
+            
+        },
+        {
+            id: 11,
+            title: "HYPER LINK ",
+            
+        },
+        {
+            id: 12,
+            title: "PLAY PLEX – GAMING ROOM",
+            
+        },
+
+
+    ],
+    
+    civil: [
+        {
+            id: 1,
+            title: "RAPID WALL",
+            
+        },
+        
+        {
+            id: 2,
+            title: "THE BEST SURVEYOR",
+            
+        },
+
+        {
+            id: 3,
+            title: "MIX AND CAST",
+           
+        },
+
+        {
+            id: 4,
+            title: "SOAPY SOCCER",
+            
+        },
+       
+        {
+            id: 5,
+            title: "FACE TO FACE ",
+            
+        },
+       
+        {
+            id: 6,
+            title: "CLASH OF CADD",
+            
+        },
+
+        {
+            id: 7,
+            title: "MINUTE TO WIN IT",
+            
+        },
+
+        {
+            id: 8,
+            title: "ESCAPE ROOM",
+            
+        },
+
+        {
+            id: 9,
+            title: "MYSTERIO(TREASURE HUNT)",
+            
+        },
+
+        {
+            id: 10,
+            title: "GRADITI 23",
+            
+        },
+        
+    ],
+
+    mech: [
+        {
+            id: 1,
+            title: "ITBP WEAPON SHOW",
+            
+        },
+        {
+            id: 2,
+            title: "LATHE MASTER",
+            
+        },
+        {
+            id: 3,
+            title: "CAD MASTER",
+            
+        },
+        {
+            id: 4,
+            title: "PROJECT PRESENTATION",
+            
+        },
+        {
+            id: 5,
+            title: "BRAIN CYCLE",
+            
+        },
+        {
+            id: 6,
+            title: "GO KART",
+            
+        },
+        {
+            id: 7,
+            title: "ENGINE EXHIBITION",
+            
+        },
+        {
+            id: 8,
+            title: "CAR DISMANTLING ",
+            
+        },
+        {
+            id: 9,
+            title: "CONTRAPTION",
+            
+        },
+    ],
+
+    electrical: [
+        {
+            id: 1,
+            title: "RC RACING",
+            
+        },
+
+        {
+            id: 2,
+            title: "ROBOTRIX",
+            
+        },
+
+        {
+            id: 3,
+            title: "SIMULINK MODELLING",
+            
+        },
+
+        {
+            id: 4,
+            title: "CIRCUIT DEBUGGING",
+            
+        },
+
+        {
+            id: 5,
+            title: "CIRCUITRIX",
+            
+
+        },
+
+        {
+            id: 6,
+            title: "MOBILE PHOTOGRAPHY",
+            
+        },
+
+        {
+            id: 8,
+            title: "TECHNICAL EXPO",
+           
+        },
+    ],
+
+    basic_science: [
+        {
+            id: 1,
+            title: "A Glimpse to KEAM- MOCK test for KEAM aspirants",
+            
+        },
+        {
+            id: 2,
+            title: "BRAIN BATTLE- Inter School Quiz Competition",
+            
+        },
+        {
+            id: 3,
+            title: "Science Safari – Working model Exhibition Competition",
+        
+        },
+    ]
+
+}
